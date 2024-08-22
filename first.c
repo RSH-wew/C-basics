2085bec0fd1e9ab1e3fec99a9473067250dc5ed4
RISHI MODI <rshmodi87@gmail.com>
2024-08-22 14:17:23 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,195 @@
+ # include<stdio.h>
+ # include<math.h>
+
+int main() {
+
+// area of circle    
+    // int radius;
+    // printf("enter your radius -> ");
+    // scanf("%d", &radius);
+    // float pi = 3.14;
+    // printf("area of circle is : %f \n", pi*radius*radius);
+
+// power and modulus
+    // int x, y;
+    // printf("enter first num -> ");
+    // scanf("%d", &x);
+    // printf("enter second num -> ");
+    // scanf("%d", &y);
+    // int z = pow(x,y);
+    // printf("remainder is : %d \n", x%y);
+
+// 3 number average
+    // int a,b,c;
+    // printf("enter first num -> ");
+    // scanf("%d", &a);
+    // printf("enter second num -> ");
+    // scanf("%d", &b);
+    // printf("enter third num -> ");
+    // scanf("%d", &c);
+    // int z = (a+b+c) / 3;
+    // printf("%d \n", z);
+
+// program to check if the value is a digit or not
+    // int a;
+    // printf("enter value -> ");
+    // scanf("%d", &a);
+    // printf("%d", a);
+
+
+// program to print a smallest number
+    // int a,b,c;
+    // printf("enter first num -> ");
+    // scanf("%d", &a);
+    // printf("enter second num -> ");
+    // scanf("%d", &b);
+    // printf("enter third num -> ");
+    // scanf("%d", &c);
+    // if (a<b && a<c) {
+    //     printf("a is smallest : %d \n", a);
+    // }
+    // else if (b<c) {
+    //     printf("b is smallest : %d \n", b);
+    // }
+    // else {
+    //     printf("c is smallest : %d \n", c);
+    // }
+
+// switch 
+    // int day;
+    // printf("enter a num -> ");
+    // scanf("%d", &day);
+    // switch (day){
+    // case 1: printf("its monday \n");
+    //     break;
+    // case 2: printf("its tuesday \n");
+    //     break;
+    // case 3: printf("its wednesday \n");
+    //     break;    
+    // default: printf("its a new day \n");
+    //     break;
+    // }
+
+//program to check if the entered num is natural or not
+    // int num;
+    // printf("enter a num -> ");
+    // scanf("%d", &num);
+    // if (num > 0 ) {
+    //     printf("it is a natural number.\n");
+    // } else if (num <= 0 ) {
+    //     printf("it is not a natural number.\n");
+    // }
+
+//for loop
+    // int i;
+    // int n;
+    // printf("enter a num -> ");
+    // scanf("%d", &n);
+    // for (int i=1 ; i <= 10 ; i+= 1) {
+    //     printf("%d\n", i*n);
+    // }
+
+//print num from 0 to 10
+    // int i=0;
+    // int n;
+    // printf("enter a num -> ");
+    // scanf("%d", &n);
+    // while (i<=n) {
+    //     printf("%d \n", i);
+    //     i++;
+    // }
+    // for(i=6; i<=n; i++) {
+    //     printf("%d \n", i);
+    // }
+        
+// sum of positive odd numbers and their squares
+    // int i;
+    // int sum = 0, sum_of_squares = 0;
+    // for (i=1; i<=30; i+=2) {
+    //     sum += i;
+    //     sum_of_squares = i*i;
+    // } printf("sum of numbers is %d \n", sum);
+    //     printf("sum of square is %d \n", sum_of_squares);
+
+//first n natural numbers (in reverse order)
+    // int i,n;
+    // int sum = 0;
+    // printf("enter a num -> ");
+    // scanf("%d", &n);
+    // for(i=n; i>=1; i-=1) {
+    //     printf("%d \n", i);
+    // }
+
+//program runs until the user enters a multiple of 7
+    // int n;
+    // do {
+    //     printf("enter a num -> ");
+    //     scanf("%d", &n);
+    //     printf("%d\n", n);
+
+    //     if (n%7 == 0) {
+    //         break;
+    //     }
+    // }while (1);
+    // printf("hehe\n");  
+
+//printing all numbers from 5 to 50 except even num
+    // int n;
+    // for(n=5; n<=50; n++){
+    //     if(n%2 == 0){
+    //         continue;
+    //     }
+    //     printf("%d \n", n);
+    // }
+
+//printing factorial of a num 
+    // int n,fact =1;
+    // printf("enter a num -> ");
+    // scanf("%d", &n);
+    // for(int i =1; i<=n; i++){
+    //     fact *= i ;
+    // }
+    // printf("%d \n", fact);
+
+//sum of 1 to 50
+    // int n, sum = 0;
+    // for(int i = 1; i<=50; i++){
+    //     sum += i ;
+    // }
+    // printf("%d \n", sum);
+
+//nested loop
+    // for(int i=0; i<5; i++){
+    //     for(int j=0; j<4; j++){
+    //         if(i==3){
+    //             break;
+    //         }
+    //     }printf("* ");
+    // }printf("\n");
+
+//program to check if a num is prime or not
+    // int n,i;
+    // printf("enter a num -> ");
+    // scanf("%d", &n);
+    // for(i=2; i<n; i++){
+    //     if(n%i==0){
+    //         printf("%d is not a prime num. \n", n);
+    //         break;
+    //     } else if(i==n){
+    //         printf("%d is a prime num. \n", n);
+    //     }
+    // }
+
+
+
+
+
+
+ 
+
+
+
+
+    return 0;
+
+}
